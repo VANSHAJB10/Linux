875b21f7ff851e6d7f1079f66e1b89c9fd42ebd8
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-11-05 23:40:01 +0530

    Update LinuxKernel.sh

--- Linux_Core_Concepts/LinuxKernel.sh
+++ Linux_Core_Concepts/LinuxKernel.sh
@@ -1,1 +1,10 @@
+# 1. bash program to print 'line 10' only when starting from line 1
 
+n=1;
+while read line ;
+do
+    if [ $n -eq 10 ]; then      # the space before $n and after 10 is necessary
+        echo "$line"
+    fi
+    n=$((n+1));
+done < file.txt
